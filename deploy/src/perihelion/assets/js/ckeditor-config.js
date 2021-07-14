e6fb9ce07c5b96f64a9a9ac0e5bdd8978f64d7d6
simon <simon@designkojo.net>
2021-07-14 14:17:33 +0900

    Update ckeditor-config.js
    remove commented code

--- deploy/src/perihelion/assets/js/ckeditor-config.js
+++ deploy/src/perihelion/assets/js/ckeditor-config.js
@@ -32,6 +32,4 @@ CKEDITOR.editorConfig = function( config ) {
     // Simplify the dialog windows.
     config.removeDialogTabs = 'image:advanced;link:advanced';
     
-    //config.startupMode = 'source';
-
 };
