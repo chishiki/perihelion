bde8c28dcd8b2434eca9c473c7826b3b3bcd5685
Christopher Webb <chishiki@gmail.com>
2021-08-03 10:32:14 +0900

    make PDO error handling stricter and give actionable feedback

--- deploy/src/perihelion/php/model/nucleus.model.php
+++ deploy/src/perihelion/php/model/nucleus.model.php
@@ -4,18 +4,23 @@ Class Nucleus {
 
     public $database;
     private static $instance;
+    private static $exception;
 
     private function __construct() {
-        
+
+    	self::$exception = null;
+
 		$dsn = 'mysql:host=' . Config::read('db.host') . ';dbname='    . Config::read('db.basename') . ';connect_timeout=15';
         $user = Config::read('db.user');            
         $password = Config::read('db.password');
-		$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
+		$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
 		try {
 			$this->database = new PDO($dsn, $user, $password, $options);
+			$this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $Exception) {
-			throw new DatabaseException($Exception->getMessage(),(int)$s->getCode());
+			self::$exception = $Exception->getMessage();
+			print_r(self::$exception);
 		}
 
     }
@@ -26,6 +31,10 @@ Class Nucleus {
             self::$instance = new $object;
         }
         return self::$instance;
+    }
+
+    public static function getException() {
+        return self::$exception;
     }
 	
 }
