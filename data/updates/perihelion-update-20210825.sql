7a03f5cef969636e2798de091f8167b9a679f55d
Christopher Webb <chishiki@gmail.com>
2021-08-27 23:31:30 +0900

    small edit to update file

--- data/updates/perihelion-update-20210825.sql
+++ data/updates/perihelion-update-20210825.sql
@@ -1,7 +1,7 @@
 /* @siramsay 2021-08-25 */
 SET @now := now();
 REPLACE INTO `perihelion_Lang` VALUES
-('select', 'Select', 0, '選択する', 0, @now);
+('select', 'Select', 0, '選択する', 0, @now),
 ('pleaseEnterValidCSS', 'Please Enter Valid CSS', 0, '有効なCSSを入力してください', 0, @now),
 ('validCSSOnly', 'valid CSS only', 0, '有効なCSSのみ', 0, @now),
 ('pleaseEnterThemeName', 'Please Enter a Theme Name', 0, 'テーマ名を入力してください', 0, @now);
