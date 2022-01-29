a7685369aeaa0a0571792134a4926e62152dbb4a
Christopher Webb <chishiki@gmail.com>
2022-01-29 23:51:00 +0900

    allow collapsible CardView->card() to default to either collapsed or not

--- deploy/src/perihelion/php/view/card.view.php
+++ deploy/src/perihelion/php/view/card.view.php
@@ -4,7 +4,7 @@ class CardView {
 
 	private $card;
 
-	public function __construct($id, $containerClasses, $breadcrumbs, $colClasses, $header, $body, $collapsible = false) {
+	public function __construct($id, $containerClasses, $breadcrumbs, $colClasses, $header, $body, $collapsible = false, $collapsed = false) {
 
 		$h = '<div id="' . $id . '" class="' . implode(' ',$containerClasses) . '">';
 			$h .= $breadcrumbs;
@@ -12,11 +12,11 @@ class CardView {
 				$h .= '<div class="' . implode(' ',$colClasses) . '">';
 					$h .= '<div class="card">';
 						$h .= '<div class="card-header"><h3>';
-							if ($collapsible) { $h .= '<a data-toggle="collapse" href="#collapse_' . $id . '" aria-expanded="true" aria-controls="collapse_' . $id . '" id="collapse_heading_' . $id . '" class="d-block">'; }
+							if ($collapsible) { $h .= '<a data-toggle="collapse" href="#collapse_' . $id . '" aria-expanded="' . ($collapsed?'false':'true') . '" aria-controls="collapse_' . $id . '" id="collapse_heading_' . $id . '" class="d-block' . ($collapsed?' collapsed':'') . '">'; }
 							$h .= $header;
 							if ($collapsible) { $h .= '</a>'; }
 						$h .= '</h3></div>';
-						if ($collapsible) { $h .= '<div id="collapse_' . $id . '" class="collapse" aria-labelledby="collapse_heading_' . $id . '">'; }
+						if ($collapsible) { $h .= '<div id="collapse_' . $id . '" class="collapse' . ($collapsed?'':' show') . '" aria-labelledby="collapse_heading_' . $id . '">'; }
 							$h .= '<div class="card-body">' . $body . '</div>';
 						if ($collapsible) { $h .= '</div>'; }
 					$h .= '</div>';
