6e1137da05e571ed175ff35de449285ee8aa41a8
Christopher Webb <chishiki@gmail.com>
2022-01-30 23:44:44 +0900

    add string utilities for converting camelcase into strings separated by hyphens or underscores

--- deploy/src/perihelion/php/model/utilities.model.php
+++ deploy/src/perihelion/php/model/utilities.model.php
@@ -205,5 +205,28 @@ final class Utilities {
 
 }
 
+final class StringUtilities {
+
+	public static function camelToUnderscore($input) {
+		$pattern = '!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!';
+		preg_match_all($pattern, $input, $matches);
+		$ret = $matches[0];
+		foreach ($ret as &$match) {
+			$match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+		}
+		return implode('_', $ret);
+	}
+
+	public static function camelToHyphen($input) {
+		$pattern = '!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!';
+		preg_match_all($pattern, $input, $matches);
+		$ret = $matches[0];
+		foreach ($ret as &$match) {
+			$match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+		}
+		return implode('-', $ret);
+	}
+
+}
 
 ?>
